544216143681f7487ea8ce412346ede23ccd638a
DaaseAllan <laurits.mumberg@gmail.com>
2016-11-02 20:55:52 +0100

    Flyvende klodser

---
+++ Topdown Game/Assets/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public Rigidbody2D rb;
+	public GameObject prefab;
+
+	void Update (){
+		if (Input.GetKey (KeyCode.W)) {
+			rb.velocity = new Vector2 (rb.velocity.x, 80);
+			Fuck ();
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			rb.velocity = new Vector2 (rb.velocity.x, -80);
+			Fuck ();
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			rb.velocity = new Vector2 (-80, rb.velocity.y);
+			Fuck ();
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			rb.velocity = new Vector2 (80, rb.velocity.y);
+			Fuck ();
+		}
+
+	}
+
+	void Fuck()
+	{
+		for (int i = 0;i < 10; i+=1) 
+		{
+			GameObject obj = Instantiate (prefab) as GameObject;
+			obj.transform.position = transform.position + new Vector3 (Random.value * 3, Random.value * 3, 0);
+			obj.GetComponent<Renderer> ().material.color = new Color (Random.value, Random.value, Random.value);
+			obj.transform.localScale = new Vector3 (Random.value, Random.value, Random.value);
+			obj.GetComponent<Rigidbody> ().AddTorque (transform.right * Time.deltaTime * 500);
+		}
+	}
+}
---
+++ Topdown Game/Assets/SmadrMaj.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmadrMaj : MonoBehaviour {
+	IEnumerator Start (){
+		yield return new WaitForSeconds (0.1f);
+		for (float i = 0; i < 3; i += Time.deltaTime) {
+			GetComponent<Renderer> ().material.color = new Color (GetComponent<Renderer> ().material.color.r, GetComponent<Renderer> ().material.color.g, GetComponent<Renderer> ().material.color.b, 1 - (i / 3));
+			print (GetComponent<Renderer> ().material.color.a);
+			yield return null;
+		}
+		Destroy (this.gameObject);
+	}
+}
